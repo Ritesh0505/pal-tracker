eca9bd88fdf81120ae14f24484e54a1d6fd3c034
rbs <ritesh.kum05@gmail.com>
2019-08-27 11:01:07 +0530

    set env var from manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,25 @@
 package io.pivotal.pal.tracker;
 
-public class HelloController {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    String msg;
+    public WelcomeController(@Value("${welcome.message}") String msg){
+    this.msg = msg;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return msg;
+    }
 }
+
+
+
+
+
+
