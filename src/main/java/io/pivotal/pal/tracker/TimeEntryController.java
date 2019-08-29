6ab487cb9d1ab30dbf677dc3076a1c99730c8b18
rbs <ritesh.kum05@gmail.com>
2019-08-29 13:32:09 +0530

    actuator

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +11,20 @@ import java.util.List;
 @RestController
 public class TimeEntryController {
 
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
     TimeEntryRepository repo ;
-    public TimeEntryController(TimeEntryRepository repo) {
+    public TimeEntryController(TimeEntryRepository repo, MeterRegistry meterRegistry) {
         this.repo=repo;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
     @PostMapping("/time-entries")
     public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
         TimeEntry timeEntry= repo.create(timeEntryToCreate);
+        actionCounter.increment();
+        timeEntrySummary.record(repo.list().size());
+
         ResponseEntity response=new ResponseEntity(timeEntry, HttpStatus.CREATED);
 
         return response ;
@@ -24,26 +34,33 @@ public class TimeEntryController {
     public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
         TimeEntry timeEntry= repo.find(id);
         ResponseEntity response;
-        if(timeEntry!=null)
+        if(timeEntry!=null) {
+            actionCounter.increment();
             return new ResponseEntity(timeEntry, HttpStatus.OK);
+        }
         response = new ResponseEntity(HttpStatus.NOT_FOUND);
         return response;
     }
     @GetMapping("/time-entries")
     public ResponseEntity<List<TimeEntry>> list() {
+        actionCounter.increment();
         return new ResponseEntity<List<TimeEntry>>(repo.list(),HttpStatus.OK);
     }
     @PutMapping("/time-entries/{id}")
     public ResponseEntity update(@PathVariable long id,@RequestBody TimeEntry expected) {
         TimeEntry entry=repo.update(id,expected);
-        if(entry!=null)
-        return new ResponseEntity<TimeEntry>(entry,HttpStatus.OK);
-
+        if(entry!=null) {
+            actionCounter.increment();
+            return new ResponseEntity<TimeEntry>(entry, HttpStatus.OK);
+        }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
     @DeleteMapping("/time-entries/{id}")
     public ResponseEntity delete(@PathVariable long id) {
         repo.delete(id);
+        actionCounter.increment();
+        timeEntrySummary.record(repo.list().size());
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
